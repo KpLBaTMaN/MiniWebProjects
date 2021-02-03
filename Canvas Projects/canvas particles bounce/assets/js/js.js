e915e61067e385e585ee1639a9d13873d948e6a8
Andrew Brown <kpl.batman123@live.co.uk>
2021-02-03 23:10:01 +0000

    completed the sheild challenge

--- Canvas Projects/canvas particles bounce/assets/js/js.js
+++ Canvas Projects/canvas particles bounce/assets/js/js.js
@@ -13,9 +13,29 @@ const numberOfParticles = 1000;
 
 let colours = ['#00FFFF', '#FF00FF', '#FFFF00'];
 
+let titleSheild = document.getElementById('titleSheild');
+let titleSheildInfo = titleSheild.getBoundingClientRect();
 
+console.log(titleSheild);
+console.log(titleSheildInfo);
+
+let title = {
+    x: titleSheildInfo.left,
+    y: titleSheildInfo.top,
+    width: titleSheildInfo.width,
+    height: titleSheildInfo.height
+}
+
+
+//class particle - used for creating cirles on the canvas
 class Particle {
 
+    /*
+     * 
+     * @param {INT} x 
+     * @param {INT} y 
+     * x and y for position on the canvas
+     */
     constructor(x, y){
         this.x = x;
         this.y = y;
@@ -27,6 +47,9 @@ class Particle {
 
     }
 
+    /*
+     * update is frame dependant on how fast the game runs
+     */
     update(){
         if(this.y > canvas.height){
             this.y = 0 - 10;
@@ -34,6 +57,8 @@ class Particle {
             this.x = Math.random() * canvas.width;
         }
 
+        //window to the other side
+        //any particle traveling from the right side will spawn on the left side
         if(this.x > canvas.width){
             this.x = 0 - 10;
         }
@@ -41,8 +66,32 @@ class Particle {
         this.weight += 0.01;
         this.y += this.weight;
         this.x += this.directionX;
+
+
+        
+
+
+        //COLLISION for title element
+        if(
+            this.x < title.x + title.width && //from point x of the title to the right of the title
+            this.x + this.size > title.x && //from the point 
+            this.y < title.y + title.height &&
+            this.y + this.size > title.y
+        ){
+            
+            
+            //full down speed - gravity 
+            this.y -=3;
+
+            //bounce
+            this.weight *= -0.5;
+
+        }
+        
     }
 
+
+    //draw each object
     draw(){
         ctx.fillStyle = this.colour;
         ctx.beginPath();
@@ -53,35 +102,74 @@ class Particle {
 }
 
 
+//creates all objects before processing
 function init(){
+    particalesArray = [];
+
+    //Produces all the particles
     for (i = 0; i < numberOfParticles; i++){
         const tempX = Math.random() * canvas.width;
         const tempY = Math.random() * canvas.height;
         particalesArray.push(new Particle(tempX, tempY));
-        console.log("Created particle: " + i);
+       //console.log("Created particle: " + i);
     }
 }
 
 
-
+//main for order to run
 function main(){
-    init();
-    animate();
+    init(); //run this first to generate the stuff
+    animate(); //runs the show
 }
 
 
 
 function animate(){
 
+    //background - colour black and fade
     ctx.fillStyle = 'rgba(0, 0, 0, 0.01)';
     ctx.fillRect(0,0, canvas.width, canvas.height);
 
+
+    //shows all the particles on the screen
     for (i = 0; i < particalesArray.length ; i++){
         particalesArray[i].draw();
         particalesArray[i].update();
     }
 
+
+    //creates the background for the title
+    ctx.fillStyle = 'rgb(200,200,200)';
+    ctx.fillRect(title.x,title.y, title.width, title.height);
+
     requestAnimationFrame(animate); //recursion
 }
 
 main();
+
+
+
+//when the user resizes the webpage - do this.
+window.addEventListener('resize', function(){
+
+    //update canvas size
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+   
+
+    //regets the title information on the title element
+    titleSheildInfo = titleSheild.getBoundingClientRect();
+    //resets the object
+    title = {
+        x: titleSheildInfo.left,
+        y: titleSheildInfo.top,
+        width: titleSheildInfo.width,
+        height: titleSheildInfo.height
+    }
+
+    //recreates the particles
+    init();
+
+    
+
+});
